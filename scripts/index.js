4b5393975409d657dc3d30fd1f1d1e739171a1d9
ryanmexin <ryan.mexin144@gmail.com>
2023-03-16 14:10:29 -0500

    JavasScript additions

--- scripts/index.js
+++ scripts/index.js
@@ -32,11 +32,61 @@ const initialCards = [
 const profileEditBtn = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
 const modalClose = document.querySelector("#profile-modal-close-btn");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__subtitle");
+const profileTitleInput = document.querySelector("#profile-title-input");
+const profileDescriptionInput = document.querySelector(
+  "#profile-description-input"
+);
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardListEl = document.querySelector("cards__list");
+const cardTemplate = document.querySelector("#card-template").content.firstElementChild;
+/*-------------------------------------------------*/
+/*                 functions                       */
+/*-------------------------------------------------*/
 
+
+function closePopup() {
+  profileEditModal.classList.remove("modal_opened");
+}
+
+
+/*-------------------------------------------------*/
+/*                Event Handlers                   */
+/*-------------------------------------------------*/
+
+function handleProfileEditSubmit(e) {
+  e.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closePopup();
+}
+/*-------------------------------------------------*/
+/* Event Listeners (to display name and subtitle in modal)*/
+/*-------------------------------------------------*/
 profileEditBtn.addEventListener("click", () => {
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
   profileEditModal.classList.add("modal_opened");
 });
 
-modalClose.addEventListener("click", () => {
-  profileEditModal.classList.remove("modal_opened");
+modalClose.addEventListener("click", closePopup);
+
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+
+
+
+initialCards.forEach((cardData) =>{
+  //clone the template element with all its content and store it in a cardElement variable
+  const cardElement = cardTemplate.cloneNode(true);
+  //access the card title and image and store them in variables
+  const cardImageEl = cardElement.querySelector(".card__image");
+  const cardTitleEl = cardElement.querySelector(".card__title");
+  //set the path to the image to the link field of the object
+  //set the image alt text to the name field of the object
+  //set the card title to the name field of the object, too
+cardTitleEl.textContent = cardData.name;
+  //return the ready HTML element with the filled-in data
+  //return cardElement;
+  cardListEl.append(cardElement);
 });
