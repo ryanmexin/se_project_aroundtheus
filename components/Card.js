37ae11c5ce531c85ba97c450426ebf58686346e7
ryanmexin <ryan.mexin144@gmail.com>
2023-04-27 10:26:49 -0500

    reviewer corrections

--- components/Card.js
+++ components/Card.js
@@ -1,70 +1,66 @@
-import{openPopUp} from "../utils/utils.js";
+import { openPopUp } from "../utils/utils.js";
 
 import {
-    previewImageModal,
-    previewImage,
-    previewImageName,
+  previewImageModal,
+  previewImage,
+  previewImageName,
 } from "../pages/index.js";
 
 export default class Card {
-    constructor({name, link}, cardSelector) {
-        this._name = name;
-        this._link = link;
-        this._cardSelector = cardSelector;
-    }
+  constructor({ name, link }, cardSelector) {
+    this._name = name;
+    this._link = link;
+    this._cardSelector = cardSelector;
+  }
 
-_setEventListeners() {
+  _setEventListeners() {
     this._cardElement
-    .querySelector(".card__like-button")
-    .addEventListener("click", () => {
+      .querySelector(".card__like-button")
+      .addEventListener("click", () => {
         this._handleLikeIcon();
-    });
+      });
     this._cardElement
-    .querySelector(".card__delete-button")
-    .addEventListener("click", () => {
+      .querySelector(".card__delete-button")
+      .addEventListener("click", () => {
         this._handleDeleteButton();
-    });
+      });
     this._cardElement
-    .querySelector(".card__image")
-    .addEventListener("click", () => {
+      .querySelector(".card__image")
+      .addEventListener("click", () => {
         this._handleCardClick();
-    });
-}
+      });
+  }
 
-_handleLikeIcon() {
+  _handleLikeIcon() {
     this._cardElement
-        .querySelector(".card__like-button")
-        .classList.toggle("card__like-button_active");
-}
-_handleDeleteButton() {
+      .querySelector(".card__like-button")
+      .classList.toggle("card__like-button_active");
+  }
+  _handleDeleteButton() {
     this._cardElement.remove();
     this._cardElement = null;
-}
+  }
 
-_handleCardClick() {
+  _handleCardClick() {
     previewImage.src = this._link;
     previewImage.alt = `Photo of ${this._name}`;
     previewImageName.textContent = this._name;
     openPopUp(previewImageModal);
-}
+  }
 
-_getTemplate() {
-      return document
-        .querySelector(this._cardSelector)
-        .content.querySelector(".card")
-        .cloneNode(true);
-    }
+  _getTemplate() {
+    return document
+      .querySelector(this._cardSelector)
+      .content.querySelector(".card")
+      .cloneNode(true);
+  }
 
-getView() {
+  getView() {
     this._cardElement = this._getTemplate();
     this._setEventListeners();
     this._cardElement.querySelector(".card__image").src = this._link;
     this._cardElement.querySelector(".card__image").alt = this._name;
     this._cardElement.querySelector(".card__title").textContent = this._name;
     return this._cardElement;
-    }
+  }
 }
-
-
-
-
--- components/FormValidator.js
+++ components/FormValidator.js
@@ -1,75 +1,71 @@
 class FormValidator {
-  constructor(settings,formEl){
-  this._inputSelector = settings.inputSelector;
-  this._submitButtonSelector = settings.submitButtonSelector;
-  this._inactiveButtonClass = settings.inactiveButtonClass;
-  this._inputErrorClass = settings.inputErrorClass;
-  this._errorClass = settings.errorClass;
-  this._form = formEl;
-}
-
-_checkInputValidity(inputEl) {
-  if(!inputEl.validity.valid) {
-      return this._showInputError(inputEl);
+  constructor(settings, formEl) {
+    this._inputSelector = settings.inputSelector;
+    this._submitButtonSelector = settings.submitButtonSelector;
+    this._inactiveButtonClass = settings.inactiveButtonClass;
+    this._inputErrorClass = settings.inputErrorClass;
+    this._errorClass = settings.errorClass;
+    this._form = formEl;
   }
-      this._hideInputError(inputEl)
-}
 
-_showInputError(inputEl) {
-  const errorMessageElement = this._form.querySelector(
+  _checkInputValidity(inputEl) {
+    if (!inputEl.validity.valid) {
+      return this._showInputError(inputEl);
+    }
+    this._hideInputError(inputEl);
+  }
+
+  _showInputError(inputEl) {
+    const errorMessageElement = this._form.querySelector(
       "#" + inputEl.id + "-error"
-  );
-inputEl.classList.add(this._inputErrorClass);
-errorMessageElement.textContent = inputEl.validationMessage;
-errorMessageElement.classList.add(this._errorClass);
-}
+    );
+    inputEl.classList.add(this._inputErrorClass);
+    errorMessageElement.textContent = inputEl.validationMessage;
+    errorMessageElement.classList.add(this._errorClass);
+  }
 
-_hideInputError(inputEl) {
-  const errorMessageElement = this._form.querySelector(
+  _hideInputError(inputEl) {
+    const errorMessageElement = this._form.querySelector(
       "#" + inputEl.id + "-error"
-  );
-  inputEl.classList.remove(this._inputErrorClass);
-  errorMessageElement.textContent = "";
-  errorMessageElement.classList.remove(this._errorClass);
+    );
+    inputEl.classList.remove(this._inputErrorClass);
+    errorMessageElement.textContent = "";
+    errorMessageElement.classList.remove(this._errorClass);
+  }
 
-}
-
-toggleButtonState() {
-  if(this._hasInvalidInput()) {
+  toggleButtonState() {
+    if (this._hasInvalidInput()) {
       this._submitButton.classList.add(this._inactiveButtonClass);
       this._submitButton.disabled = true;
       return;
+    }
+    this._submitButton.classList.remove(this._inactiveButtonClass);
+    this._submitButton.disabled = false;
   }
-  this._submitButton.classList.remove(this._inactiveButtonClass);
-  this._submitButton.disabled = false;
-}
 
-_hasInvalidInput() {
-  return !this._inputEls.every(
-     (inputEl) => inputEl.validity.valid 
-  );
-}
+  _hasInvalidInput() {
+    return !this._inputEls.every((inputEl) => inputEl.validity.valid);
+  }
 
-_setEventListeners() {
-  this._inputEls = [...this._form.querySelectorAll(this._inputSelector)];
-  this._submitButton = this._form.querySelector(this._submitButtonSelector);
-  this.toggleButtonState();
-  this._inputEls.forEach((inputEl) => {
+  _setEventListeners() {
+    this._inputEls = [...this._form.querySelectorAll(this._inputSelector)];
+    this._submitButton = this._form.querySelector(this._submitButtonSelector);
+    this.toggleButtonState();
+    this._inputEls.forEach((inputEl) => {
       inputEl.addEventListener("input", (evt) => {
-         this._checkInputValidity(inputEl);
-         this.toggleButtonState(); 
+        this._checkInputValidity(inputEl);
+        this.toggleButtonState();
       });
-  });
-}
+    });
+  }
 
-enableValidation() {
-  this._form.addEventListener("submit", (evt) => {
+  enableValidation() {
+    this._form.addEventListener("submit", (evt) => {
       evt.preventDefault();
-  });
-  this._setEventListeners();
-  this.toggleButtonState();
-}
+    });
+    this._setEventListeners();
+    
+  }
 }
 
 export default FormValidator;
-
